6ff8a4409939e701dd03fd8576b48c9b7b502973
Swapnil Kulkarni <kulkarni.swapnil.v@gmail.com>
2019-12-12 22:36:36 +0530

    Added sample program for Factory Method

---
+++ Factory/FactoryMethod.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<sstream>
+#include <corecrt_math_defines.h>
+
+#define _USE_MATH_DEFINES
+
+enum class PontType
+{
+	catesian, polar
+};
+class Point
+{
+public:
+
+	friend std::ostream & operator << (std::ostream & os, Point point)
+	{
+		return os << "x: " << point.x << " y: " << point.y;
+	}
+	static Point NewCartesian(double x, double y)
+	{
+		return { x,y };
+	}
+
+	static Point NewPolar(double r, double theta)
+	{
+		return { r*cos(theta),r*sin(theta) };
+	}
+
+private:
+	double x, y;
+	Point(const double & x, const double & y)
+		:x{ x },
+		y{ y }
+	{
+
+	}
+};
+
+int main()
+{
+	auto pc = Point::NewCartesian(1, 2);
+	std::cout << pc << std::endl;
+
+	auto pp = Point::NewPolar(5, M_PI_4);
+	std::cout << pp << std::endl;
+
+	getchar();
+	return 0;
+}
+
+//output:
+//
+//x: 1 y : 2
+//x : 3.53553 y : 3.53553
---
+++ SOLID Principles/DependancyInversion.cpp
@@ -0,0 +1,3 @@
+//Dependancy inversion principle 
+//High level module should not be depend on low level module but, both should depend on abstractions
+//Abstraction should not depend upon details, but details should depend upon abstraction
