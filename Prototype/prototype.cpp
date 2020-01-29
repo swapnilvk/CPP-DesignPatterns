4bee966a2b5c5f87be9001a854697f4d78f1e89d
Swapnil Kulkarni <kulkarni.swapnil.v@gmail.com>
2020-01-29 14:55:27 +0530

    Added program for prototype pattern

---
+++ Prototype/prototype.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+struct Address
+{
+	string street;
+	string city;
+	int suit;
+
+	Address(const string& s, const string& c)
+	{
+		street = s;
+		city = c;
+	}
+
+	friend ostream& operator << (ostream& os, const Address& obj)
+	{
+		return os << "Street: " << obj.street << "City: " << obj.city << "Suit: " << obj.suit;
+	}
+};
+
+struct Contact
+{
+	string name;
+	Address* work_address;
+	
+	Contact(const string& n, Address* a)
+	{
+		name = n;
+		work_address = a;
+	}
+
+	~Contact()
+	{
+		//delete work_address;
+	}
+
+	friend ostream& operator << (ostream& os, const Contact & obj)
+	{
+		return os << "Name: " << obj.name << "Work address" << *obj.work_address;
+	}
+};
+
+int main()
+{ 
+	Address* addr = new Address("M G Road", "Pune"); //prototype
+	Contact  swapnil("Swapnil Kulkarni", addr);
+	swapnil.work_address->suit = 100;
+	Contact  smruti("Smruti Kulkarni", addr);
+	smruti.work_address->suit = 200;
+	cout << swapnil << endl << smruti << endl;
+
+	delete addr;
+
+	getchar();
+
+	return 0;
+}
